420e940883eb67267859a8c807b010e2cfa13c3a
horodeski <geovana.horodeski06@gmail.com>
2024-01-08 19:41:31 -0300

    FEAT NAVBAR: Criando efeito no background da navbar

--- src/components/common/nav/Nav.jsx
+++ src/components/common/nav/Nav.jsx
@@ -1,9 +1,24 @@
 import Dropdown from "./dropdown";
 import styles from "./nav.module.css";
+import { useState, useEffect } from "react";
 
 const Nav = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      setIsScrolled(scrollTop > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <nav>
+    <nav className={isScrolled ? styles.scrolled : ""}>
       <img
         src="../../../../public/LojasP10/logo-menor-nome.png"
         alt="logo das lojas P10"
